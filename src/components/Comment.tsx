bb45b4888e7f096c4da6f1016302221ec5306c07
Muma Dev <muma.sanmartin2011@gmail.com>
2022-12-24 14:07:32 -0300

    add comment fetch

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,7 +1,16 @@
-const Comment = () => {
+
+
+type componentProps = {
+    title: string
+}
+
+const Comment = (props : componentProps) => {
+    console.log(props)
     return (
         <div>
             This is a single comment
+            <p>{props.title}</p>
+            
         </div>
     )
 }
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,23 +1,41 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 import Comment from "./Comment"
+import { IComment } from "../interfaces/comments"
 
 const Comments = () => {
+
+    const [comments, setComments] = useState<any>(null)
 
     useEffect(() => {
         const getComments = async () => {
             const {data} = await axios('http://localhost:3000/comment/')
-            console.log(data)
+            setComments(data)
         }
         getComments()
     }, [])
 
+    if(comments !== null){
+        return (
+            <div>
+    
+                {comments.map((comment: IComment) => {
+                    return <Comment title={comment.comment}/>
+                })}
+    
+            </div>
+        ) 
+    }
+    console.log(3, 'withOutComments')
+
     return (
         <div>
-            <Comment/>
+            <h1>No Comments</h1>
         </div>
     )
+
+
 }
 
 
