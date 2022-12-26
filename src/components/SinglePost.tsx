b4fbc0c17e4196922ca4bff346dc9e884d44adfb
Muma Dev <muma.sanmartin2011@gmail.com>
2022-12-25 21:14:19 -0300

    add material and likes prop

--- src/components/SinglePost.tsx
+++ src/components/SinglePost.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import {IPost} from '../interfaces/post'
+import KeyboardArrowUpRoundedIcon from '@mui/icons-material/KeyboardArrowUpRounded';
 
 type props = {
     post : IPost
@@ -10,10 +11,13 @@ function SinglePost({post} : props){
     return (
         <div className="post-container">
             <div className="image-holder"><img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" alt="Placerholder image" /></div>
-            <div className="post-icon-likes">Icon</div>
+            <div className="post-icon-likes">
+                <KeyboardArrowUpRoundedIcon/>
+                <p>{post.number_likes}</p>         
+            </div>
             <div className="post-title-container">
                 <Link to={`post/${post.id}`}>{post.title}</Link>
-                <p>{post.first_name} {post.last_name} · {post.count} comments</p> 
+                <p>{post.first_name} {post.last_name} · {post.number_comments} comments</p> 
             </div>
         </div>
     )
--- src/interfaces/post.ts
+++ src/interfaces/post.ts
@@ -2,7 +2,8 @@ export interface IPost{
     id: number
     title: string
     created_at: Date
-    count: string
+    number_comments: string
+    number_likes: string
     first_name: string
     last_name: string
 }
