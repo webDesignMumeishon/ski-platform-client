039b1ca8530db2b9ad5dadc3fcb6a2314578d59b
Muma Dev <muma.sanmartin2011@gmail.com>
2022-12-27 21:33:56 -0300

    add user login class

---
+++ src/request/UserLogin.ts
@@ -0,0 +1,21 @@
+
+class UserLogin {
+    private _email : string
+    private _password: string
+
+    constructor(email : string, password: string){
+        this._email = email
+        this._password = password
+    }
+
+    get email(){
+        return this._email
+    }
+
+    get password(){
+        return this._password
+    }
+}
+
+
+export default UserLogin
