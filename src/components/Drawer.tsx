5f4fffa2812a98d90bed34c45928ca0544456735
Muma Dev <muma.sanmartin2011@gmail.com>
2022-12-31 19:10:24 -0300

    relocate menu drawer icon

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -13,8 +13,7 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Outlet, useNavigate } from "react-router-dom"
 
 
@@ -45,7 +44,7 @@ function ResponsiveDrawer(props: Props) {
 
   const drawer = (
     <div>
-      <Toolbar />
+      {/* <Toolbar /> */}
       <p color='#243648' style={{color: '#f7f7f88c', padding: '10px'}}>{capitalizeFirstLetter(center)}, {capitalizeFirstLetter(state)}</p>
       <Divider sx={{backgroundColor: '#f7f7f88c'}}/>
       <List>
@@ -81,32 +80,18 @@ function ResponsiveDrawer(props: Props) {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box>
+    <IconButton
+      color="inherit"
+      aria-label="open drawer"
+      edge="start"
+      onClick={handleDrawerToggle}
+      sx={{ mr: 2, display: { sm: 'none' }, left: '5%' }}
+    >
+      <MenuIcon />
+    </IconButton>
+    <Box sx={{ display: 'flex', alignItems: 'baseline' }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          // width: { sm: `calc(100% - ${drawerWidth}px)` },
-          // ml: { sm: `${drawerWidth}px` },
-          backgroundColor: '#005A9C',
-          boxShadow: 'none'
-        }}
-      >
-        <Toolbar sx={{minHeight: '45px !important'}}>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Link style={{fontSize: '16px', color: '#fff'}} to='/' className=''>
-            Ski Resorts
-          </Link>
-        </Toolbar>
-      </AppBar>
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
@@ -169,6 +154,7 @@ function ResponsiveDrawer(props: Props) {
 
       </Box>
     </Box>
+  </Box>
   );
 }
 
