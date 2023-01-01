17c113edccd02c2bb8206afdd8330f384952300f
Muma Dev <muma.sanmartin2011@gmail.com>
2023-01-01 08:29:59 -0300

    add missing item prop to grid

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,29 +5,38 @@ const Navbar = () => {
     
     return (
         <Grid className="navbar-container" container alignItems='center' justifyContent={'space-between'}>
+
             <Grid item lg={1} style={{marginRight: '10px'}}>
                 <p >ICON</p>
             </Grid>
 
-            <Grid xs={8} md={8} sm={7}>
+            <Grid item xs={8} md={8} sm={7}>
+
                 <Grid container className="nav-bar-wrapper">
-                    <Grid xs={4} className='search-input'>
+
+                    <Grid item xs={4} className='search-input'>
                         <label htmlFor="Search">
                             <input id="Search" placeholder="Search"/>
                         </label>
                     </Grid>
-                    <Grid>
+
+                    <Grid item>
                         <Link to='/'>Home</Link>
                     </Grid>
-                    <Grid>
+
+                    <Grid item>
                         <Link to='/'>Resorts</Link>
                     </Grid>
+
                 </Grid>
+
             </Grid>
 
-            <Grid container lg={1} md={1} sm={2}>
-                <Grid>
-                    <Link to='/login'>Login</Link>
+            <Grid item lg={1} md={1} sm={2}>
+                <Grid container>
+                    <Grid item>
+                        <Link to='/login'>Login</Link>
+                    </Grid>
                 </Grid>
             </Grid>
 
