3771c1f25f388984649028697714a80508358343
Muma Dev <muma.sanmartin2011@gmail.com>
2023-01-19 19:44:50 -0300

    testing

--- src/request/Like.ts
+++ src/request/Like.ts
@@ -1,15 +1,13 @@
-import { ILikeRequest } from "../interfaces/like"
+import { ILikeRequest } from '../interfaces/like'
 
-export class LikeRequest implements ILikeRequest{
-    private _postId : number
+export class LikeRequest implements ILikeRequest {
+  private readonly _postId: number
 
-    constructor(postId: number){
-        this._postId = postId
-    }
+  constructor (postId: number) {
+    this._postId = postId
+  }
 
-    get postId(){
-        return this._postId;
-    }
+  get postId (): number {
+    return this._postId
+  }
 }
-
-
