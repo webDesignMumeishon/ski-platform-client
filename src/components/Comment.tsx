f45084c0569d9d17e7fb9c52bd3a84e181a1a115
Muma Dev <muma.sanmartin2011@gmail.com>
2023-01-19 18:06:56 -0300

    set up config file

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,56 +1,53 @@
-import { IComment } from "../interfaces/comments"
+import { IComment } from "../interfaces/comments";
 
 type componentProps = {
-    comment: IComment
-    replies: IComment[] | []
-}
+  comment: IComment;
+  replies: IComment[] | [];
+};
 
 type Reply = {
-    reply: IComment
-}
+  reply: IComment;
+};
 
-const Replies =  ({reply} : Reply) => {
-    return (
-        <div className="reply-container">
-            <div className="reply-photo">
-                <img src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="Photo" />
-            </div>
+const Replies = ({ reply }: Reply) => {
+  return (
+    <div className="reply-container">
+      <div className="reply-photo">
+        <img
+          src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
+          alt="Photo"
+        />
+      </div>
 
-            <div className="reply-content">
-                <p>
-                    {reply.text}
-                </p>
-            </div>
-        </div>
-    )
-}
+      <div className="reply-content">
+        <p>{reply.text}</p>
+      </div>
+    </div>
+  );
+};
 
-
-const Comment = ({comment, replies} : componentProps) => {
-
-    return (
-        <div className="comment-container">
-            {/* <div className="comment-image-container">
+const Comment = ({ comment, replies }: componentProps) => {
+  return (
+    <div className="comment-container">
+      {/* <div className="comment-image-container">
                 <img src="https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png" alt="Placeholder" />
             </div> */}
-            <div className="comment-content">
-                <div className="comment-header">
-                    <p><span>{comment.first_name}</span> commented on {comment.created_at}</p>
-                </div>
-
-                <div className="comment-main">
-                    {comment.text}
-                </div>
-
-                {replies.length > 0 && 
-                replies.map(reply => {
-                    return <Replies reply={reply}/>
-                })
-                }
-                
-            </div>
+      <div className="comment-content">
+        <div className="comment-header">
+          <p>
+            <span>{comment.first_name}</span> commented on {comment.created_at}
+          </p>
         </div>
-    )
-}
 
-export default Comment
+        <div className="comment-main">{comment.text}</div>
+
+        {replies.length > 0 &&
+          replies.map((reply) => {
+            return <Replies reply={reply} />;
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
