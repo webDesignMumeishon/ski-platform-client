c6b57e9c5ce2e832285823690368e43e0c1de92f
Muma Dev <muma.sanmartin2011@gmail.com>
2023-01-19 19:45:41 -0300

    testing

--- src/components/Discussion.tsx
+++ src/components/Discussion.tsx
@@ -1,16 +1,15 @@
-import Comments from './Comments';
-import DiscussionSideBar from './DiscussionSideBar';
-import DiscussionTitle from './DiscussionTitle';
-
+import Comments from "./Comments";
+import DiscussionSideBar from "./DiscussionSideBar";
+import DiscussionTitle from "./DiscussionTitle";
 
 function Discussion() {
   return (
-    <div className='discussion-container'>
-        <DiscussionTitle/>
-        <div className='discussion-bucket'>
-            <Comments/>
-            <DiscussionSideBar/>
-        </div>
+    <div className="discussion-container">
+      <DiscussionTitle />
+      <div className="discussion-bucket">
+        <Comments />
+        <DiscussionSideBar />
+      </div>
     </div>
   );
 }
