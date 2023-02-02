25be0bc6e5a2f10ea278726cebe82f1e53befec9
Muma Dev <muma.sanmartin2011@gmail.com>
2023-02-01 21:59:15 -0300

    Fix handleWords

--- src/utils/handleWords.ts
+++ src/utils/handleWords.ts
@@ -1,8 +1,8 @@
-export  const capitalizeFirstLetter = (word : string | undefined) => {
-    if(typeof word === 'string'){
-      const leftPart = word[0].toUpperCase()
-      const rightPart = word.slice(1)
-      return leftPart + rightPart
-    }
-    return ''
+export const capitalizeFirstLetter = (word: string | undefined): string => {
+  if (typeof word === 'string') {
+    const leftPart = word[0].toUpperCase()
+    const rightPart = word.slice(1)
+    return leftPart + rightPart
+  }
+  return ''
 }
