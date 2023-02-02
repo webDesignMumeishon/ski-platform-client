87be5b605f9f674de4c60e409e8d9c4153ddf701
Muma Dev <muma.sanmartin2011@gmail.com>
2023-02-01 23:20:59 -0300

    Fix eslint

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,19 +1,14 @@
-import { AxiosResponse } from 'axios';
-
 import ServerFacade from "../network/ServerFacade";
 import Login from '../request/Login';
 import { IUserLogged } from '../interfaces/user';
 import { User } from '../response/User';
 
 class UserService {
-
 	// GET
-	static async userLogin(email: string, password: string): Promise<User> {
+	static async userLogin (email: string, password: string): Promise<User> {
         const login = new Login(email, password)
-        return await ServerFacade.userLogin<IUserLogged>(login);
+        return await ServerFacade.userLogin<IUserLogged>(login)
 	}
-
- 
 }
 
 export default UserService;
