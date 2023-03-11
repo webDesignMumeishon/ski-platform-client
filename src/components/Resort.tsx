614356c3ab02c60f3cdcadf454363f34ff6fa9d7
Muma Dev <muma.sanmartin2011@gmail.com>
2023-03-11 15:53:31 -0300

    Capitalize first letter in resorts

--- src/components/Resort.tsx
+++ src/components/Resort.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { Grid } from "@mui/material";
 import { FaSnowboarding } from 'react-icons/fa';
 
+import { capitalizeFirstLetter } from "../utils/handleWords";
 import { IResort } from "../interfaces/resort";
 
 
@@ -10,6 +11,7 @@ type PropsType = {
 }
 
 const Resort = ({resort} : PropsType) => {
+
     return (
         <Grid container lg={2} md={3} sm={4} xs={12} className='resort-container' justifyContent={'space-between'}>
             <Grid item  xs={1}>
@@ -18,7 +20,7 @@ const Resort = ({resort} : PropsType) => {
 
             <Grid item  xs={11}>
                 <Link to={`/${resort.state}/${resort.city}/report`} replace={false}>
-                    {resort.city}, {resort.state}
+                    {capitalizeFirstLetter(resort.city)}, {capitalizeFirstLetter(resort.state)}
                 </Link>
             </Grid>
         </Grid>
