cbd629bddbb3571535c851eaeb508fb1aaceac43
Muma Dev <muma.sanmartin2011@gmail.com>
2023-03-12 21:17:46 -0300

    add fetching to component

--- src/components/Report.tsx
+++ src/components/Report.tsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react"
 import { Box } from "@mui/system"
+import { useParams } from "react-router-dom";
+
 import ResortReport from './ResortReport'
+import ResortService from "../service/ResortService"
+import {IResortReport} from '../interfaces/resort'
 
 const Report = () => {
+    const { state, center } = useParams();
+
+    const [report, setReport] = useState<IResortReport>({
+        openLifts: '',
+        openTerrain: '',
+        openTrails: '',
+        snowConditions: ''
+    })
+
+
+
+
+    useEffect(() => {
+        const fetchData = async () : Promise<void> => {
+            if(state !== undefined && center !== undefined){
+                const response = await ResortService.getResortReport(state, center)
+                setReport(response.data)
+            }
+        }
+        fetchData()
+    }, [])
+
     return (
         <Box>
-            <ResortReport area={{title: 'Area Open', value: '88%'}} trails={{title: 'Trails Open', value: '67 of 76'}}/>
+            <ResortReport 
+                area={{title: 'Area Open', value: report.openTerrain}} 
+                trails={{title: 'Trails Open', value: report.openTrails}}
+                lift={{title: 'Lifts Open', value: report.openLifts}}
+                condition={{title: 'Conditions', value: report.snowConditions}}
+            />
         </Box>
     )
 }
