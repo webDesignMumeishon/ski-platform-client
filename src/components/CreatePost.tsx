68f45de7d47b64dcf0908fb25617828f1229483a
Muma Dev <muma.sanmartin2011@gmail.com>
2023-04-17 22:49:33 -0300

    some styling to create post component

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,23 +1,45 @@
-import { Grid, Box, TextareaAutosize} from "@mui/material";
+import { Grid, Box, TextField} from "@mui/material";
+import { HiLocationMarker } from 'react-icons/hi';
+
+
 const CreatePost = () => {
     return (
         <div>
             <Box >
-                <Grid container direction={'column'}>
+                <h2>Create Post</h2>
+                <Grid container direction={'column'} sx={{width: '60%', margin: 'auto'}} spacing={2}>
+
                     <Grid item>
-                    <TextareaAutosize     
-                        aria-label="empty textarea"
-                        placeholder="Enter text here"
-                        maxRows={10}
-                        minRows={10} 
-                    />
+                        <TextField id="outlined-basic" label="Title" variant="outlined" />
                     </Grid>
+               
+                    <Grid item sx={{}}>
+                        <TextField
+                            sx={{width: "100%", margin: 'auto',display: 'flex'}}
+                            id="outlined-multiline-static"
+                            multiline
+                            label="Post"
+                            rows={5}
+                            placeholder="Create a great post today..."
+                        />
+                    </Grid>
+
+                    <Grid item>
+                        <Grid container direction="row" alignItems="center">
+                            <Grid item>
+                                <HiLocationMarker />
+                            </Grid>
+                            <Grid item sx={{marginLeft: '1em'}}>
+                                <p>Add Location</p>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+
                     <Grid item>
                         <button>Comment</button>
                     </Grid>
                 </Grid>
             </Box>
- 
         </div>
     )
 }
