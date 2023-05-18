59d63f3dd8ce40972dec62a05cf7be55de565dcb
Muma Dev <muma.sanmartin2011@gmail.com>
2023-05-17 22:32:36 -0300

    fetch user in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
 
+import { useAppDispatch } from "./redux/hooks";
+import { fetchUser } from "./redux/slices/user";
 import './App.css';
 import Navbar from './components/Navbar'
 
 function App() {
+  const dispatch = useAppDispatch()
+  dispatch(fetchUser())
   return (
     <div>
       <Navbar/>
