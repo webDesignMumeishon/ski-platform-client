70e827678ff95be41ebf0dafdc5fd847e383ddfc
Muma Dev <muma.sanmartin2011@gmail.com>
2023-05-17 22:35:11 -0300

    add styling and service

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,14 +1,17 @@
 import ServerFacade from "../network/ServerFacade";
+import UserFacade from "../network/UserFacade";
 import Login from '../request/Login';
 import { User } from '../response/User';
 
 class UserService {
 	// GET
 	static async userLogin (email: string, password: string): Promise<User> {
-
         const login = new Login(email, password)
-		
         return await ServerFacade.userLogin(login)
+	}
+
+	static async getUser(): Promise<User> {
+        return await UserFacade.getUser()
 	}
 }
 
