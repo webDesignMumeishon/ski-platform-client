b84177633fb9e43d71cfdc336febd2b9dd5d81f7
Muma Dev <muma.sanmartin2011@gmail.com>
2023-05-20 11:52:35 -0300

    basic structure of map working

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,84 @@
+import {
+  MapContainer,
+  TileLayer,
+  Polygon,
+  // Popup
+} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import { statesData } from './const/usa'
+// import { useNavigate } from "react-router-dom";
+import BasicModal from './Modal';
+import { useState } from 'react';
 
 type ComponentProps = object
 
-function Main( {} : ComponentProps){
-    return (
-        <h1>Main</h1>
-    )
+
+function Main({ }: ComponentProps) {
+  const [open, setOpen] = useState(false);
+  const [state, setState] = useState('')
+
+  // const navigate = useNavigate();
+
+
+  return (
+    <div>
+      <BasicModal open={open} setOpen={setOpen} state={state}/>
+      <MapContainer center={[39.8283, -98.5795]} zoom={4}>
+        <TileLayer
+          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+          url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=DEhmp08z0P2tXJ9G5ij4"
+        />
+        {
+          statesData.features.map((state: any) => {
+            const coordinates: any = state.geometry.coordinates[0].map((item: any) => [item[1], item[0]]);
+            return (
+              <Polygon
+                key={state.id}
+                pathOptions={{
+                  fillColor: '#FD8D3C',
+                  fillOpacity: 0.7,
+                  weight: 2,
+                  opacity: 1,
+                  dashArray: '3',
+                  color: 'white'
+                }}
+                positions={coordinates}
+                eventHandlers={{
+                  mouseover: (e) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      dashArray: "",
+                      fillColor: "#BD0026",
+                      fillOpacity: 0.7,
+                      weight: 2,
+                      opacity: 1,
+                      color: "white",
+                    })
+                  },
+                  mouseout: (e: any) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      fillOpacity: 0.7,
+                      weight: 2,
+                      dashArray: "3",
+                      color: 'white',
+                      fillColor: '#FD8D3C'
+                    });
+                  },
+                  click: () => {
+                    setState(state.properties.name)
+                    setOpen(!open)
+                    // navigate(`/${state.properties.state}/${state.properties.town}/report`);
+                  }
+                }}
+              />)
+          })
+        }
+      </MapContainer>
+    </div>
+
+  )
 }
 
 export default Main
+
---
+++ src/components/Modal.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+type Props = {
+    open: boolean
+    state: string
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const style = {
+  position: 'absolute' as const,
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function BasicModal({open, setOpen, state} : Props) {
+//   const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {state}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
