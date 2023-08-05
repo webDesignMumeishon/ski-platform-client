c0d5db0af52aee1c324b3de963e65348961b8abf
Muma Dev <muma.sanmartin2011@gmail.com>
2023-08-05 14:58:12 -0300

    add missing item

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,30 +1,32 @@
-import { useState } from 'react'
-import { Grid } from '@mui/material';
+import { useState } from "react";
+import { Grid } from "@mui/material";
 
-
-import { IComment } from '../interfaces/comments'
-import {getFullDate} from '../utils/getDate'
-import { CommentsStateProps } from './Comments';
-import CreateCommentReply from './CreateCommentReply';
-import Reply from './Reply';
+import { IComment } from "../interfaces/comments";
+import { getFullDate } from "../utils/getDate";
+import { CommentsStateProps } from "./Comments";
+import CreateCommentReply from "./CreateCommentReply";
+import Reply from "./Reply";
 import MainButton from "./Button";
 
-
 type Props = {
-  comment: IComment
-  index: number
-  replies: IComment[] | []
-  setComments: React.Dispatch<React.SetStateAction<CommentsStateProps>>
-}
+  comment: IComment;
+  index: number;
+  replies: IComment[] | [];
+  setComments: React.Dispatch<React.SetStateAction<CommentsStateProps>>;
+};
 
-const Comment = ({ comment, replies, index, setComments }: Props): JSX.Element => {
-
-  const [showReply, setShowReply] = useState<boolean>(false)
+const Comment = ({
+  comment,
+  replies,
+  index,
+  setComments,
+}: Props): JSX.Element => {
+  const [showReply, setShowReply] = useState<boolean>(false);
 
   const handleShowReply = () => {
-    setShowReply(!showReply)
-  }
-  const isFirstComment = index === 0
+    setShowReply(!showReply);
+  };
+  const isFirstComment = index === 0;
 
   function compareFn(a: IComment, b: IComment) {
     if (a.id < b.id) {
@@ -34,16 +36,21 @@ const Comment = ({ comment, replies, index, setComments }: Props): JSX.Element =
       return 1;
     }
     return 0;
-  } 
+  }
 
-  replies.sort(compareFn)
+  replies.sort(compareFn);
 
   return (
-    <Grid container className="comment-container" style={{paddingTop: isFirstComment ? '0px' : ''}}>
-      <Grid className="comment-content" xs={12}>
-        <Grid item  xs={12} className="comment-header">
+    <Grid
+      container
+      className="comment-container"
+      style={{ paddingTop: isFirstComment ? "0px" : "" }}
+    >
+      <Grid className="comment-content" item xs={12}>
+        <Grid item xs={12} className="comment-header">
           <p>
-            <span>{comment.first_name}</span> commented on {getFullDate(new Date(comment.created_at))}
+            <span>{comment.first_name}</span> commented on{" "}
+            {getFullDate(new Date(comment.created_at))}
           </p>
         </Grid>
 
@@ -53,22 +60,21 @@ const Comment = ({ comment, replies, index, setComments }: Props): JSX.Element =
 
         {replies.length > 0 &&
           replies.map((reply) => {
-            return <Reply key={reply.id} reply={reply} />
-          })
-        }
+            return <Reply key={reply.id} reply={reply} />;
+          })}
 
-        {
-          showReply
-          ?
-          <CreateCommentReply parentId={comment.id} handleShowReply={handleShowReply} setComments={setComments}/>
-          :
+        {showReply ? (
+          <CreateCommentReply
+            parentId={comment.id}
+            handleShowReply={handleShowReply}
+            setComments={setComments}
+          />
+        ) : (
           <MainButton callback={handleShowReply}>Reply</MainButton>
-        }
+        )}
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-
-
-export default Comment
+export default Comment;
